24e530e4840e52e707bb9e45aedc017f28892ae5
Никита Раненко <nranenko@bk.ru>
2022-11-26 00:11:39 +0700

    Запуск отправки сообщений

--- MessagesGenerator/Program.cs
+++ MessagesGenerator/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MessagesGenerator
+﻿using MessagesGenerator.Services;
+using RabbitMQ.Client;
+
+namespace MessagesGenerator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            var factory = new ConnectionFactory()
+            { 
+                HostName = "95.163.241.218", 
+                Port = Protocols.DefaultProtocol.DefaultPort, 
+                UserName = "root", 
+                Password = "root",
+                VirtualHost = "/",
+                AutomaticRecoveryEnabled = true,
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            MessagesSender messagesSender = new(channel, TimeSpan.FromSeconds(1));
+
+            Console.ReadKey();
         }
     }
 }
