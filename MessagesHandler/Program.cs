fc87afa78225cbfd6e556996871ef1c7242d5017
Никита Раненко <nranenko@bk.ru>
2022-11-26 00:12:29 +0700

    Реализовано чтение очереди сообщений

--- MessagesHandler/Program.cs
+++ MessagesHandler/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace MessagesHandler
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace MessagesHandler
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "95.163.241.218",
+                Port = Protocols.DefaultProtocol.DefaultPort,
+                UserName = "root",
+                Password = "root",
+                VirtualHost = "/",
+                AutomaticRecoveryEnabled = true,
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+            };
+
+            while (true)
+            {
+                channel.BasicConsume(queue: "messagesWithPriorities",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Thread.Sleep(1000);
+            }
         }
     }
 }
