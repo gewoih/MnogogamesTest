b3bda982630f770e10232717b5c8d5cd0376c853
Никита Раненко <nranenko@bk.ru>
2022-11-25 21:02:03 +0700

    project init

---
+++ MessagesHandler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MessagesHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
