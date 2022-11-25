0d6640402113022dd01918dee5aa8fb7952ef19a
Никита Раненко <nranenko@bk.ru>
2022-11-26 00:10:52 +0700

    Модель сообщения

---
+++ SharedLibrary/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using SharedLibrary.Enums;
+
+namespace SharedLibrary.Models
+{
+    public sealed class Message
+    {
+        public readonly Guid Id;
+        public readonly DateTime CreationDate;
+        public readonly string Data;
+
+        public Message(string data)
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+            Data = data;
+        }
+    }
+}
