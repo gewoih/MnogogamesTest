63086b5bcdad6f4348bc5e371a173eee69026a02
Никита Раненко <nranenko@bk.ru>
2022-11-26 00:09:34 +0700

    Статический класс для генерации случайных строк и enum'ов

---
+++ MessagesGenerator/Services/MessagesGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace MessagesGenerator.Services
+{
+    internal static class MessagesGenerator
+    {
+        private readonly static Random _random = new();
+
+        public static string GetRandomString(int length = 6)
+        {
+            const string charsSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            return new string(Enumerable.Repeat(charsSet, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+
+        public static T GetRandomEnumValue<T>() where T : Enum
+        {
+            var enumValuesArray = Enum.GetValues(typeof(T));
+            return (T)enumValuesArray.GetValue(_random.Next(enumValuesArray.Length));
+        }
+    }
+}
