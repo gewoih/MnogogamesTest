6272e1b7e98f2d6ed0d9b60e51bade6d3dcd6bdc
Никита Раненко <nranenko@bk.ru>
2022-11-26 00:10:00 +0700

    Класс для отправки сообщений в rabbitMq с определенным интервалом

---
+++ MessagesGenerator/Services/MessagesSender.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using SharedLibrary.Enums;
+using SharedLibrary.Models;
+using System.Text;
+
+namespace MessagesGenerator.Services
+{
+    internal sealed class MessagesSender
+    {
+        private readonly TimeSpan _sendingInterval;
+        private readonly IModel _rabbitmqModel;
+
+        public MessagesSender(IModel rabbitMqModel, TimeSpan sendingInterval)
+        {
+            _rabbitmqModel = rabbitMqModel;
+            _sendingInterval = sendingInterval;
+
+            Timer timer = new(MessagesSenderHandler, null, TimeSpan.FromSeconds(0), _sendingInterval);
+        }
+
+        private void MessagesSenderHandler(object obj)
+        {
+            string generatedString = MessagesGenerator.GetRandomString();
+            var generatedPriority = MessagesGenerator.GetRandomEnumValue<Priority>();
+            Message generatedMessage = new(generatedString, generatedPriority);
+
+            SendMessageToRabbiqMQ(generatedMessage, generatedPriority);
+
+            Thread.Sleep(_sendingInterval);
+        }
+
+        private void SendMessageToRabbiqMQ(Message message, Priority priority)
+        {
+            IBasicProperties properties = _rabbitmqModel.CreateBasicProperties();
+            properties.Priority = (byte)priority;
+
+            string serializedMessage = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(serializedMessage);
+
+            _rabbitmqModel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: "messagesWithPriorities",
+                basicProperties: properties,
+                body: body);
+        }
+    }
+}
